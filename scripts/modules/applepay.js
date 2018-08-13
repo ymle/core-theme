f887201fd1388105fe0249e3d031ac3fb03d973e
shelkeller <michele.keller@kibocommerce.com>
2018-08-13 13:48:43 -0500

    who even knows...

--- scripts/modules/applepay.js
+++ scripts/modules/applepay.js
@@ -1,16 +1,24 @@
 define(['modules/jquery-mozu','modules/eventbus',"modules/api",'hyprlivecontext','underscore'],
 function($,EventBus, Api, hyprlivecontext, _) {
   var ApplePaySession = window.ApplePaySession;
-  var initialized = false;
   var ApplePay = {
     init: function(style){
-      if (!initialized){
-        initialized = true;
+        var self = this;
+        // configure button with selected style and language
         this.setStyle(style);
         this.setLanguage();
+
+        /*
+          canMakePayments passes if:
+          - the user is on the most recent version of Safari on OSX sierra or a recent iPad
+          - the user has a wallet set up on an iPhone (must be iPhone - not iPad)
+        */
         if (ApplePaySession && ApplePaySession.canMakePayments()){
             $("#applePayButton").show();
             $("#applePayButton").on('click', function(event){
+
+              //TODO: populate this request with accurate information
+
               var request = {
                 countryCode: 'US',
                 currencyCode: 'USD',
@@ -19,15 +27,17 @@ function($,EventBus, Api, hyprlivecontext, _) {
                 merchantCapabilities: ['supports3DS']
               };
 
+              // define our ApplePay Session with the version number
               self.session = new ApplePaySession(3, request);
+
+              // set handlers. These all get called by apple.
               self.session.onvalidatemerchant = function(event){
                   var validationURL = event.validationURL;
-                  console.log(J({'validationURL': validationURL}));
                   $.ajax({
                       type: 'POST',
-                      url: 'https://shelkeller.ngrok.io/merchant-session/new',
-                      data: $.params({'validationURL': validationURL}),
+                      url: 'https://shelkeller.ngrok.io/merchant-session/new?validationURL='+validationURL+'&domainName='+window.location.hostname,
                       dataType: 'json',
+                      contentType: 'application/json',
                       success: function(response){
                           console.log(response);
                           self.session.completeMerchantValidation(response);
@@ -37,9 +47,20 @@ function($,EventBus, Api, hyprlivecontext, _) {
                       }
                   });
               };
+
+
               self.session.onpaymentmethodselected = function(event){
-                  console.log("on payment method selected");
-                  self.session.completePaymentMethodSelection(event);
+                  console.log(event);
+                  self.session.completePaymentMethodSelection(
+                    {
+                      newTotal: {
+                        "label": "NewTotal Label",
+                        "amount": "10.00",
+                        "type": "final"
+                      },
+                      newLineItems: []
+                    }
+                  );
               };
               self.session.onshippingmethodselected = function(event){
                   console.log("on shipping method selected");
@@ -49,6 +70,20 @@ function($,EventBus, Api, hyprlivecontext, _) {
                   console.log("on shipping contact selected");
                   self.session.completeShippingContactSelected(event);
               };
+              self.session.onpaymentauthorized = function(event) {
+                console.log("on payment authorized");
+                console.log(event);
+                //event holds onto the payment token.
+
+                //TODO: We have to set the status of the authorization manually.
+                // We'll be getting information from the event object if there is a problem
+                // so we need to figure out what that will look like and how to handle it.
+                var result = self.session.completePayment({"status": 0});
+                console.log(result);
+              }
+              //TODO: define oncancel handler
+
+              //
               self.session.begin();
             });
             // remove hidden class from button
@@ -59,7 +94,7 @@ function($,EventBus, Api, hyprlivecontext, _) {
               $("#applePayButton").hide();
           }
         }
-      }
+
 
     },
     setStyle: function(style){
